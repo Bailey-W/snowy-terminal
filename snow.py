7b9389bd649c7ed5a22feffc32a261fe71040fe1
NerosJourney <sandman4264@gmail.com>
2022-10-20 12:47:29 -0400

    added rain and snow options

--- snow.py
+++ snow.py
@@ -13,19 +13,21 @@ def buildScreen(n, m):
         res.append(col)
     return res
 
-def printScreen(screen, char1, char2):
+def printScreen(screen, char1, char2, char3=None):
     for row in screen:
         for col in row:
             if col == 0:
                 print(char1, end="")
+            elif col == 1:
+                print(char2, end="")
             else:
-                print(char2, end=""),
+                print(char3, end=""),
         print("")
 
-def createSnow(screen):
+def createSnow(screen, percentage):
     for pixel in range(len(screen[0])):
-        r = random.randint(1, len(screen[0]))
-        if r == 1:
+        r = random.random()*100
+        if r <= percentage:
             screen[0][pixel] = 1
 
 def updateSnow(screen):
@@ -48,16 +50,40 @@ def updateSnowStack(screen):
                     if screen[row+1][col] == 0:
                         screen[row+1][col] = 1
                         screen[row][col] = 0
+                    else:
+                        screen[row][col] = 2
+                        screen[row+1][col] = 2
+                else:
+                    screen[row][col] = 2
 
-def loop(screen):
+def snow(n, m, speed=4, freq=5):
+    screen = buildScreen(n, m)
     while True:
         clear()
-        printScreen(screen, " ", "*")
-        # screen = updateSnow(screen)
+        printScreen(screen, " ", "*", u"\u2588")
         updateSnowStack(screen)
-        createSnow(screen)
-        time.sleep(0.05)
+        createSnow(screen, freq)
+        time.sleep(1 / speed)
 
 
-snowScreen = buildScreen(20, 50)
-loop(snowScreen)
+def rain(n, m, speed=20, freq=4):
+    screen = buildScreen(n, m)
+    while True:
+        clear()
+        printScreen(screen, " ", "|")
+        updateSnow(screen)
+        createSnow(screen, freq)
+        time.sleep(1 / speed)
+
+
+if __name__ == "__main__":
+    print("Choose:\n  1. Snow\n  2. Rain")
+    choice = int(input())
+    print("Speed?: ")
+    speed = float(input())
+    print("Frequency?: ")
+    freq = float(input())
+    if choice == 1:
+        snow(30, 100, speed, freq)
+    else:
+        rain(30, 100, speed, freq)
